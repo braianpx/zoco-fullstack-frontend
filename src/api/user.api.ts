eedce0db7b2968b51e23314d953be4587309077e
braianpx <braivaldez16@gmail.com>
2026-02-16 03:00:40 -0300

    feat: agreagar nuevo endpoint user

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,8 +1,18 @@
 import type { ApiResponse } from "../types/apiResponse.types";
-import type { UserCreate, UserResponse } from "../types/user.types";
+import type { UserCreate, UserResponse, UserUpdate } from "../types/user.types";
 import { api } from "./axios";
 
 export const register = async (data: UserCreate) => {
   const res = await api.post<ApiResponse<UserResponse>>("users", data);
+  return res.data;
+};
+
+export const getUserDetail = async (userId: number) => {
+  const res = await api.get<ApiResponse<UserResponse>>("users/"+ userId);
+  return res.data;
+};
+
+export const updateUser = async (data: UserUpdate, userId: number) => {
+  const res = await api.put<ApiResponse<UserResponse>>("users/"+ userId, data);
   return res.data;
 };
