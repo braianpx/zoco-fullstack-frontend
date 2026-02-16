803b1a0efcd92e131649693a2918aa018e86dda6
braianpx <braivaldez16@gmail.com>
2026-02-16 08:12:38 -0300

    feat: agregar endpoints study

---
+++ src/api/study.api.ts
@@ -0,0 +1,29 @@
+import type { ApiResponse } from "../types/apiResponse.types";
+import type { StudyCreate, StudyResponse, StudyUpdate } from "../types/study.types";
+import { api } from "./axios";
+
+export const createStudy = async (data: StudyCreate) => {
+  data.endDate = null
+  const res = await api.post<ApiResponse<StudyResponse>>("studies", data);
+  return res.data;
+};
+
+export const getStudiesUser = async (userId: number) => {
+  const res = await api.get<ApiResponse<StudyResponse>>("studies/user/"+ userId);
+  return res.data;
+};
+
+export const getAllStudies = async () => {
+  const res = await api.get<ApiResponse<StudyResponse[]>>("studies");
+  return res.data;
+};
+
+export const updateStudies = async (data: StudyUpdate, userId: number) => {
+  const res = await api.put<ApiResponse<StudyResponse>>("studies/"+ userId, data);
+  return res.data;
+};
+
+export const deleteStudies = async ( userId: number) => {
+  const res = await api.delete<ApiResponse<null>>("studies/"+ userId);
+  return res.data;
+};
