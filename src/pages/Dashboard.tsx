886d29f7df119016b777bc79b4f918521c4529f4
braianpx <braivaldez16@gmail.com>
2026-02-15 21:24:56 -0300

    feat: agreagar archivo Dashboard

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { type FC } from "react";
+
+export const Dashboard: FC = () => {
+  return (
+    <div className="h-full w-full bg-[#F9FAFB] flex">
+      <h1>HOLA DASH</h1>
+    </div>
+  );
+};
