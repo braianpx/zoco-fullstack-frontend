d765a256c3cf7a7d70ec9e64ab39c7e0738feb36
braianpx <braivaldez16@gmail.com>
2026-02-15 22:36:46 -0300

    feat: agregar layout dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,26 @@
-import { type FC } from "react";
+// src/pages/Dashboard.tsx
+import { type FC, useState } from "react";
+import { Sidebar } from "../components/Sidebar";
+import { useAuth } from "../context/auth/useAuth";
+import { Outlet } from "react-router-dom";
 
 export const Dashboard: FC = () => {
+  const { role } = useAuth();
+  const [selectedSection, setSelectedSection] = useState<string>("Perfil");
+
   return (
-    <div className="h-full w-full bg-[#F9FAFB] flex">
-      <h1>HOLA DASH</h1>
+    <div className="flex h-full bg-[#F9FAFB]">
+      {/* Sidebar */}
+      <Sidebar
+        role={role}
+        selectedSection={selectedSection}
+        onSelectSection={setSelectedSection}
+      />
+
+      {/* Main Content */}
+      <div className="flex-1 bg-white shadow-inner rounded-l-2xl">
+        <Outlet />
+      </div>
     </div>
   );
 };
